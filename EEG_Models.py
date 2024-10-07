081868e6a6aa9e43cf133f1d09fbf8b5521652e9
NNEdelweiss <nhi.nguyenpb@gmail.com>
2024-10-07 23:05:39 +0200

    remove import wrappers

--- EEG_Models.py
+++ EEG_Models.py
@@ -3,13 +3,12 @@ from tensorflow.keras.models import Model, Sequential # type: ignore
 from tensorflow.keras.layers import (   # type: ignore
     Dense, Activation, Permute, Dropout, Conv2D, MaxPooling2D, Conv3D, MaxPooling3D,
     AveragePooling2D, SeparableConv2D, DepthwiseConv2D, BatchNormalization,
-    SpatialDropout2D, Input, Flatten, Reshape, LSTM, TimeDistributed,
+    SpatialDropout2D, Input, Flatten, Reshape, LSTM, TimeDistributed, 
     Bidirectional, RepeatVector, Conv1D, MaxPool1D, GlobalAveragePooling1D, MaxPooling1D,
     Add, multiply, Concatenate, Reshape, concatenate, GRU, Lambda, LeakyReLU, Multiply
 ) 
 from tensorflow.keras.regularizers import l1_l2 # type: ignore
 from tensorflow.keras.constraints import max_norm # type: ignore
-from keras.layers.wrappers import Bidirectional # type: ignore
 from tensorflow.keras import backend as K, utils as np_utils, regularizers # type: ignore
 from tensorflow.keras.optimizers import Adam, RMSprop # type: ignore 
 from sklearn import svm, neighbors, discriminant_analysis
